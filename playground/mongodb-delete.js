16163d99e0aea25355341e39944f6018d0ccb32a
Chris Massengale <chris.massengale@verizon.net>
2018-08-23 11:44:24 -0400

    add delete script for mongo

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,62 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+
+//.connect(url, callback)
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
+  if(error) {
+    return console.log('Unable to connect to MongoDB server!!');
+  }
+
+  console.log('Connected to MongoDB server')
+ 
+  //deleteMany
+  // db.collection('Todos').deleteMany({text: "Eat Lunch"}).then((result) => {
+  //   console.log(result);
+  // },(error) => {
+
+  // });
+
+  //deleteOne
+  // db.collection('Todos').deleteOne({text: "Eat Lunch"}).then((result) => {
+  //   console.log(result);
+  // }, (error) => {
+
+  // });
+
+  //findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({completed: false}).then((result) =>{
+  //   console.log(result);
+  // }, (error) => {
+
+  // });
+
+  //Challenge -- 
+  // --DELETE all records with name: Chris
+  // --DELETE one by ID
+
+  //deleteMany
+  // db.collection('Users').deleteMany({name: 'Chris'}).then((result) => {
+  //   console.log(result);
+  // }, (error) => {
+
+  // });
+
+  //deleteOne (use _ID)
+  // db.collection('Users').deleteOne({_id: new ObjectID('5b7ebf48015957751eb45550')}).then((result) => {
+  //   console.log(result);
+  // }, (error) =>{
+
+  // });
+
+  db.collection('Users').findOneAndDelete({_id: new ObjectID('5b7ebf18ca6c247513e34f2d')}).then((result) => {
+    console.log(result);
+  }, (error) =>{
+    console.log('Unable to delete!');
+  });
+
+  //db.close(); //Will interupt deletions
+
+});
