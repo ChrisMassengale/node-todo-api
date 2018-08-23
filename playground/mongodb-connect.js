1eea9bc510d02dd704873e58b2d4a705e0fe29d6
Chris Massengale <chris.massengale@verizon.net>
2018-08-23 09:57:51 -0400

    Add record to Mongo using Mongo Cllient

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,37 @@
+const MongoClient = require('mongodb').MongoClient;
+
+//.connect(url, callback)
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {//if use Mongo 3.o must use 'client' instead of 'db' in callback function
+  if(error) {
+    return console.log('Unable to connect to MongoDB server!!');
+  }
+
+  console.log('Connected to MongoDB server')
+  //const db = client.db('TodoApp');  //ADD (for Mongo V3)>  
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to Do',
+  //   completed: false
+  // } , (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert todo', error)
+  //   }
+
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'Chris',
+    age: 51,
+    location: 'Rehoboth Beach, DE'
+  }, (error, result) =>{
+    if(error) {
+      return console.log("Unable to Add a User", error);
+    }
+
+    console.log(JSON.stringify(result.ops,undefined, 2));
+  });
+
+  db.close(); //client.close()  //Use teh client.close() for MongoDb v3
+
+});
