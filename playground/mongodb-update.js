a3832f04533718fd304f03793c1b1affcab583d9
Chris Massengale <chris.massengale@verizon.net>
2018-08-23 14:38:30 -0400

    update challenge

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -13,11 +13,23 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
   console.log('Connected to MongoDB server')
  
   //findOneAndUpdate
-  db.collection('Todos').findOneAndUpdate(
-    {_id: new ObjectID('5b7ecec472ff721b1f0394bf')}, 
-    { $set: {text: 'meet the neighbors'}},
-    { returnOriginal: false}
-  ).then((result) => console.log(result));
+  // db.collection('Todos').findOneAndUpdate(
+  //   {_id: new ObjectID('5b7ecec472ff721b1f0394bf')}, 
+  //   { $set: {text: 'meet the neighbors'}},
+  //   { returnOriginal: false}
+  // ).then((result) => console.log(result));
+
+  //Try with Users table -  in the challenge use $inc to increase age by 1 year
+  db.collection('Users').findOneAndUpdate(
+      {_id: new ObjectID('5b7efb8572ff721b1f0394e4')},  //Obj - filter/criteria
+      {$inc: {'age': 1}},  //2nd Parameter is and Object with changes to take place
+      {returnOriginal: false } //last parameter is options.
+  ).then((result) => {
+    console.log(result);
+  },
+  (error) => {
+
+  });
 
   //db.close(); //Will interupt deletions
 
