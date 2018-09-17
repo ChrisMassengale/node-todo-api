d2dc529832d0abbac3b946166304f8f9547e2487
Chris Massengale <chris.massengale@verizon.net>
2018-09-17 17:56:07 -0400

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -33,6 +34,31 @@ app.get('/todos', (request, response) => {
     response.status(400).send(e);
   })
 });
+
+//individual Todo -->  GET /todo/:id
+app.get('/todos/:id', (request, response) => {
+    var id = request.params.id;
+
+    //IsValid
+    //false - 404 - send back empty
+    if (!ObjectID.isValid(id)) {
+      return response.status(404).send('404 Not Found (Line 45)');
+    }
+ 
+    Todo.findById(id).then((todo)=>{
+        if (!todo) {
+          return response.status(404).send('404 Not Found (Line 50)');
+        }
+        response.status(200).send({todo});
+      }).catch((e) => {
+        response.status(400).send('400 Error (Line 54)');
+      });
+    //findById
+      //success - send todo
+      // if no todo - send back 404 with empth body
+      //fail = 400 - send empty body back empty
+});
+
 
 app.listen(3000, () => {
   console.log('started on port 3000');
