a33a23882081236da22b346768382cf3887a8cb9
Chris Massengale <chris.massengale@verizon.net>
2018-09-19 08:29:40 -0400

    Add PATCH /TODOS/:ID

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash')
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -78,10 +79,37 @@ app.delete('/todos/:id', (request, response) => {
   }).catch( (err) => {
     response.status(400).send('400 Error line 71');
   });
-
-  
 });
 
+//PATCH
+app.patch('/todos/:id', (request, response) => {
+
+  var id = request.params.id;
+  var body = _.pick(request.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return response.status(404).send('404 Not found (line 67)');
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, {new: true}).then( (todo) => {
+    if (!todo) {
+      return response.status(404).send('404 Not Found (line 105')
+    }
+
+    response.status(200).send({todo});
+  }).catch((e) => {
+    response.status(400).send('400 Error line')
+  });
+});
 
 app.listen(port, () => {
   console.log(`started on port ${port}`);
